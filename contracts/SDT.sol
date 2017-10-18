8e396c24734df28b2dabc2315591a66f567e4afe
Marcio Abreu <3432864+maabreuc@users.noreply.github.com>
2017-10-17 22:24:26 -0500

    Add constructor to SDT

--- contracts/SDT.sol
+++ contracts/SDT.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.4.15;
 import './SendToken.sol';
 
 contract SDT is SendToken {
-  string public name = "SEND Token";
-  string public symbol = "SDT";
-  uint256 public decimals = 18;
+	string public name = "SEND Token";
+	string public symbol = "SDT";
+	uint256 public decimals = 18;
+
+	function SDT(uint256 _supply){
+		balances[msg.sender] = _supply;
+		totalSupply = _supply;
+	}
+
 }
