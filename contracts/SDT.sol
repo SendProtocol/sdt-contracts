1eed9396573fe5b4f30851b6b3b7a888daaa2cd6
Marcio Abreu <3432864+maabreuc@users.noreply.github.com>
2017-10-18 16:36:16 -0500

    fix decimal places on constructor

--- contracts/SDT.sol
+++ contracts/SDT.sol
@@ -8,8 +8,9 @@ contract SDT is SendToken {
 	uint256 public decimals = 18;
 
 	function SDT(uint256 _supply){
-		balances[msg.sender] = _supply;
-		totalSupply = _supply;
+		uint256 supply = _supply*10**decimals;
+		balances[msg.sender] = supply;
+		totalSupply = supply;
 	}	
 
 }
