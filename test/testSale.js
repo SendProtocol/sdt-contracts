24e97e2504379b3d4ec1722fea408c206a2d3ba5
Klaus Hott Vidal <klahott@gmail.com>
2017-11-28 15:46:38 +1300

    :art: using prettierjs to standardize syntax of js and json files (tests and configs).

--- test/testSale.js
+++ test/testSale.js
@@ -1,109 +1,105 @@
-'use strict';
+"use strict";
 
-const TokenSale = artifacts.require('./TokenSale.sol');
-const SDT = artifacts.require('./SDT.sol');
+const TokenSale = artifacts.require("./TokenSale.sol");
+const SDT = artifacts.require("./SDT.sol");
 const TokenVesting = artifacts.require('./TokenVesting.sol');
-const assertJump = require('./helpers/assertJump');
-const math = require('mathjs');
+const assertJump = require("./helpers/assertJump");
+const math = require("mathjs");
 
-contract('TokenSale', function(accounts) {
-	let sale;
-    let vesting;
-	let token;
-	let bought;
-	let error;
-	let maxError = 0.00001;
-	let currentDate = math.floor(Date.now() / 1000);
+contract("TokenSale", function(accounts) {
+  let sale;
+  let vesting;
+  let token;
+  let bought;
+  let error;
+  let maxError = 0.00001;
+  let currentDate = math.floor(Date.now() / 1000);
 
-    it('should fail if not active', async function() 
-    {
-        sale = await TokenSale.deployed();
-        vesting = await TokenVesting.deployed();
-		try {
-			await sale.purchase(10, 0, 0, 0x1, 100, currentDate + 5000, 0);
-			assert.fail('should have thrown before');
-		} catch(error) {
-			assertJump(error);
-		}
-    });
+  it("should fail if not active", async function() {
+    sale = await TokenSale.deployed();
+    vesting = await TokenVesting.deployed();
+    try {
+      await sale.purchase(10, 0, 0, 0x1, 100, currentDate + 5000, 0);
+      assert.fail("should have thrown before");
+    } catch (error) {
+      assertJump(error);
+    }
+  });
 
-    it('should fail if not owner', async function() 
-    {
-		try {
-			await sale.deploy(700*10**6, 0, 0x20, {from: accounts[1]});
-			assert.fail('should have thrown before');
-		} catch(error) {
-			assertJump(error);
-		}
-    });
+  it("should fail if not owner", async function() {
+    try {
+      await sale.deploy(700 * 10 ** 6, 0, vesting.address, { from: accounts[1] });
+      assert.fail("should have thrown before");
+    } catch (error) {
+      assertJump(error);
+    }
+  });
 
-    it('should be possible to activate crowdsale', async function() 
-    {
-        await sale.deploy(700*10**6, 0, vesting.address);
-        assert (sale.activated.call());
+  it("should be possible to activate crowdsale", async function() {
+    await sale.deploy(700 * 10 ** 6, 0, vesting.address);
+    assert(sale.activated.call());
 
-    	let _tokenAddress = await sale.token.call();
-    	token = await SDT.at(_tokenAddress);
+    let _tokenAddress = await sale.token.call();
+    token = await SDT.at(_tokenAddress);
 
-        vesting.init(_tokenAddress, sale.address)
+    vesting.init(_tokenAddress, sale.address);
 
-    	assert.equal(await token.balanceOf.call(sale.address), 700 * 10 ** 24);
-    });
+    assert.equal(await token.balanceOf.call(sale.address), 700 * 10 ** 24);
+  });
 
-    it('should fail if purchasing less than min', async function() 
-    {
-		try {
-			await sale.purchase(9, 0, 0, 0x1, 100, currentDate + 5000, 0);
-			assert.fail('should have thrown before');
-		} catch(error) {
-			assertJump(error);
-		}
-    });
+  it("should fail if purchasing less than min", async function() {
+    try {
+      await sale.purchase(9, 0, 0, 0x1, 100, currentDate + 5000, 0);
+      assert.fail("should have thrown before");
+    } catch (error) {
+      assertJump(error);
+    }
+  });
 
-    it('10 USD at 0.14 - should return the right amount with a maximum error of 0.001%', async function() 
-    {
-        bought = await sale.computeTokens(10);
-        await sale.purchase(10, 0, 0, 0x1, 100, currentDate + 5000, 0);
-        error = math.abs(bought.valueOf() - 10/0.14 * 10**18);
+  it("10 USD at 0.14 - should return the right amount with a maximum error of 0.001%", async function() {
+    bought = await sale.computeTokens.call(10);
+    await sale.purchase(10, 0, 0, 0x1, 100, currentDate + 5000, 0);
+    error = math.abs(bought.valueOf() - 10 / 0.14 * 10 ** 18);
 
-        assert(error < bought.valueOf() * maxError);
-    });
+    assert(error < bought.valueOf() * maxError);
+  });
 
-    it('6M USD at 0.14 - should return right amount with a maximum error of 0.001%', async function() 
-    {
-    	bought = await sale.computeTokens(6000000);
-        await sale.purchase(6000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
-        error = math.abs(bought.valueOf() - 6000000/0.14 * 10**18);
+  it("6M USD at 0.14 - should return right amount with a maximum error of 0.001%", async function() {
+    bought = await sale.computeTokens.call(6000000);
+    await sale.purchase(6000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
+    error = math.abs(bought.valueOf() - 6000000 / 0.14 * 10 ** 18);
 
-        assert(error < bought.valueOf() * maxError);
-    });
+    assert(error < bought.valueOf() * maxError);
+  });
 
-    it('2M USD with 6M and 10 USD sold,' +
-    	'should return 999990 USD 0.14 and 1000010 with incremental price formula,' +
-    	'with a maximum error of 0.001%', async function() 
-    {
-        let val1 = 999990/0.14 * 10**18;
-        let val2 = 70000000 * math.log(1.07142928571) * 10 ** 18;
+  it(
+    "2M USD with 6M and 10 USD sold," +
+      "should return 999990 USD 0.14 and 1000010 with incremental price formula," +
+      "with a maximum error of 0.001%",
+    async function() {
+      let val1 = 999990 / 0.14 * 10 ** 18;
+      let val2 = 70000000 * math.log(1.07142928571) * 10 ** 18;
 
-    	bought = await sale.computeTokens(2000000);
-        await sale.purchase(2000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
+      bought = await sale.computeTokens.call(2000000);
+      await sale.purchase(2000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
 
-        error = math.abs(bought.valueOf() - val1 - val2);
+      error = math.abs(bought.valueOf() - val1 - val2);
 
-        assert(error < bought.valueOf() * maxError);
-    });
+      assert(error < bought.valueOf() * maxError);
+    }
+  );
 
-    it('7M USD with 8M and 10 USD sold, should return the right amout ' +
-    	'with a maximum error of 0.001%', async function() 
-    {
-    	let val1 = 70000000 * math.log(1.46666635556) * 10 ** 18;
+  it(
+    "7M USD with 8M and 10 USD sold, should return the right amout " +
+      "with a maximum error of 0.001%",
+    async function() {
+      let val1 = 70000000 * math.log(1.46666635556) * 10 ** 18;
 
-    	bought = await sale.computeTokens(7000000);
-        await sale.purchase(7000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
-        error = math.abs(bought.valueOf() - val1);
+      bought = await sale.computeTokens.call(7000000);
+      await sale.purchase(7000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
+      error = math.abs(bought.valueOf() - val1);
 
-        assert(error < bought.valueOf() * maxError);
-    });
-
-
+      assert(error < bought.valueOf() * maxError);
+    }
+  );
 });
--- test/testTokenBasic.js
+++ test/testTokenBasic.js
@@ -1,43 +1,45 @@
-'use strict';
+"use strict";
 
-const SDT = artifacts.require('./SDT.sol');
-const assertJump = require('./helpers/assertJump');
-const math = require('mathjs');
+const SDT = artifacts.require("./SDT.sol");
+const assertJump = require("./helpers/assertJump");
+const math = require("mathjs");
 
-contract('SDT', function(accounts) {
+contract("SDT", function(accounts) {
+  it("contract deploy should put 700M * 10^18 SDT in the first account", async function() {
+    let token = await SDT.new(700 * 10 ** 6, accounts[0], accounts[1], 100);
+    let balance = await token.balanceOf.call(accounts[0]);
+    let expectedBalance = 7 * math.pow(10, 26);
+    assert.equal(
+      balance.valueOf(),
+      expectedBalance,
+      "700M * 10^18 wasn not in the first account"
+    );
+  });
 
-    it('contract deploy should put 700M * 10^18 SDT in the first account', async function() {
-        let token = await SDT.new(700*10**6, accounts[0], accounts[1], 100);
-        let balance = await token.balanceOf.call(accounts[0]);
-        let expectedBalance = 7 * math.pow(10,26);
-        assert.equal(balance.valueOf(), expectedBalance, '700M * 10^18 wasn not in the first account');
+  describe("consensus network", function() {
+    let token;
+
+    it("should be verified by default", async function() {
+      token = await SDT.new(1, accounts[0], accounts[1], 100);
+      assert(await token.isVerified(accounts[0]));
     });
 
-	describe('consensus network', function() {
-		let token;
+    it("should be unverified by default", async function() {
+      assert(!await token.isVerified(accounts[1]));
+    });
 
-		it('should be verified by default', async function() {
-			token = await SDT.new(1, accounts[0], accounts[1], 100);
-			assert(await token.isVerified(accounts[0]));
-		});
+    it("should possible to verify", async function() {
+      await token.verify(accounts[1]);
+      assert(await token.isVerified(accounts[1]));
+    });
 
-		it('should be unverified by default', async function() {
-			assert(!(await token.isVerified(accounts[1])));
-		});
-
-		it('should possible to verify', async function() {
-			await token.verify(accounts[1]);
-			assert(await token.isVerified(accounts[1]));
-		});
-
-		it('should not be possible to verify', async function() {
-			try {
-				await token.verify(accounts[2], {from: accounts[1]});
-				assert.fail('should have thrown before');
-			} catch(error) {
-				assertJump(error);
-			}
-		});
-	});
-
+    it("should not be possible to verify", async function() {
+      try {
+        await token.verify(accounts[2], { from: accounts[1] });
+        assert.fail("should have thrown before");
+      } catch (error) {
+        assertJump(error);
+      }
+    });
+  });
 });
