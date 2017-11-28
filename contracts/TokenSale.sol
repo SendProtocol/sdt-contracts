444d0e762a7f606f9e1040beeabd115ebdcaddc0
Klaus Hott Vidal <klahott@gmail.com>
2017-11-28 15:37:43 +1300

    :art: standardizing whitespace and syntax for solidity files.

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -8,277 +8,267 @@ import './TokenVesting.sol';
  * @dev see https://send.sd/crowdsale
  */
 contract TokenSale {
+  uint256 public startTime;
+  uint256 public endTime;
+  address public ownerWallet;
+  address public foundationWallet;
+  address public corpotationWallet;
+  address public rewardWallet;
+  address public teamWallet;
 
-	uint256 public startTime;
-	uint256 public endTime;
-	address public ownerWallet;
-	address public foundationWallet;
-	address public corpotationWallet;
-	address public rewardWallet;
-	address public teamWallet;
+  uint256 public startVesting;
 
-	uint256 public startVesting;
+  uint256 public soldTokens = 0;
+  uint256 public raised = 0;
+  bool public activated = false;
+  bool public isStopped = false;
+  bool public isFinalized = false;
 
-	uint256 public soldTokens = 0;
-	uint256 public raised = 0;
-	bool public activated = false;
-	bool public isStopped = false;
-	bool public isFinalized = false;
+  SDT public token;
+  TokenVesting public vesting;
 
-	SDT public token;
-	TokenVesting public vesting;
+  event NewBuyer(
+    address indexed holder,
+    uint256 sndAmount,
+    uint256 usdAmount,
+    uint256 ethAmount,
+    uint256 btcAmount
+  );
 
-    event NewBuyer(
-    	address indexed holder, 
-    	uint256 sndAmount, 
-    	uint256 usdAmount, 
-    	uint256 ethAmount, 
-    	uint256 btcAmount
+  modifier validAddress(address _address){
+    require(_address != address(0x0));
+    _;
+  }
+
+  modifier isActive(){
+    require(activated == true);
+    require(isStopped == false);
+    require(isFinalized == false);
+    require(block.timestamp >= startTime);
+    require(block.timestamp <= endTime);
+    _;
+  }
+
+  modifier isOwner(){
+    require(msg.sender == ownerWallet);
+    _;
+  }
+
+  function TokenSale(
+      uint256 _startTime,
+      uint256 _endTime,
+      address _foundationWallet,
+      address _corporationWallet,
+      address _rewardWallet,
+      address _teamWallet,
+      uint256 _startVesting
+  )
+      public
+      validAddress(_foundationWallet)
+      validAddress(_corporationWallet)
+      validAddress(_rewardWallet)
+      validAddress(_teamWallet)
+  {
+    require(_startTime > block.timestamp - 60);
+    require(_endTime > startTime);
+    require(_startVesting > startTime);
+
+    startVesting = _startVesting;
+    startTime = _startTime;
+    endTime = _endTime;
+    ownerWallet = msg.sender;
+    foundationWallet = _foundationWallet;
+    corpotationWallet = _corporationWallet;
+    rewardWallet = _rewardWallet;
+    teamWallet = _teamWallet;
+  }
+
+  /**
+   * @dev deploy the token itself
+   * @notice The owner of this contract is the owner of token's contract
+   * @param _supply Token total supply
+   * @param _ownerPool Percentage of tokens the owner assigns to himself
+   */
+  function deploy(
+      uint256 _supply,
+      uint256 _ownerPool,
+      address _vestingContract
+  ) public isOwner returns(bool) {
+    require(!activated);
+    token = new SDT(
+      _supply,
+      msg.sender,
+      this,
+      _ownerPool
     );
+    vesting = TokenVesting(_vestingContract);
+    activated = true;
+    return true;
+  }
 
-    modifier validAddress(address _address){
-        require(_address != address(0x0));
-        _;
+  function stop() public isOwner isActive returns(bool) {
+    isStopped = true;
+    return true;
+  }
+
+  function resume() public isOwner returns(bool) {
+    require(isStopped);
+    isStopped = false;
+    return true;
+  }
+
+  /**
+   * @dev deploy the token itself
+   * @notice The owner of this contract is the owner of token's contract
+   * @param _usd amount invested in USD
+   * @param _eth amount invested in ETH y contribution was made in ETH, 0 otherwise
+   * @param _btc amount invested in BTC y contribution was made in BTC, 0 otherwise
+   * @param _address Address to send tokens to
+   * @param _vesting vesting finish timestamp
+   * @param _discountBase a multiplier for tokens based on a discount choosen and a vesting time
+   * @param _purchaseVestingStarts Vesting start timestamp
+   */
+  function purchase(
+      uint256 _usd,
+      uint256 _eth,
+      uint256 _btc,
+      address _address,
+      uint256 _discountBase,
+      uint256 _vesting,
+      uint256 _purchaseVestingStarts
+  )
+      public
+      isActive
+      isOwner
+      validAddress(_address)
+      returns(uint256)
+  {
+    require(_usd >= 10);
+
+    uint256 soldAmount = computeTokens(_usd);
+    soldAmount = computeBonus(soldAmount, _discountBase);
+
+    if (_purchaseVestingStarts < startVesting){
+      _purchaseVestingStarts = startVesting;
     }
 
-    modifier isActive(){
-        require(activated == true);
-        require(isStopped == false);
-        require(isFinalized == false);
-        require(block.timestamp >= startTime);
-        require(block.timestamp <= endTime);
-        _;
+    grantVestedTokens(
+      _address,
+      soldAmount,
+      _purchaseVestingStarts,
+      _vesting
+    );
+    updateStats(_usd, soldAmount);
+    NewBuyer(_address, soldAmount, _usd, _eth, _btc);
+
+    return soldAmount;
+  }
+
+  /**
+   * @dev Helper function to update collected and allocated tokens stats
+   */
+  function updateStats(uint256 usd, uint256 tokens) internal {
+    raised = raised + usd;
+    soldTokens = soldTokens + tokens;
+  }
+
+  /**
+   * @dev Helper function to compute bonus amount
+   * @param _amount number of toknes before bonus
+   * @param _discountBase percentage of price after discount
+   * @notice 80 <= dicountBase <= 100
+   * @notice _discountBase is the resultant of (100 - discount)
+   */
+  function computeBonus(
+      uint256 _amount,
+      uint256 _discountBase
+  ) internal pure returns(uint256) {
+    require(_discountBase >= 80);
+    require(_discountBase <= 100);
+    return _amount * 100 / _discountBase;
+  }
+
+  /**
+  * @dev Number of tokens is given by:
+  * 70,000,000 ln((7,000,000 + raised + usd) / (7,000,000 + raised))
+  */
+  function computeTokens(uint256 usd) public view returns(uint256) {
+    require(usd > 0);
+
+    uint256 _numerator;
+    uint256 _denominator;
+
+    if (raised < 7000000) {
+      if (usd + raised > 7000000) {
+
+        uint256 _usd = 7000000 - raised;
+        usd -= _usd;
+
+        _denominator = 14000000; // 7M+7M raised
+        _numerator = _denominator + usd;
+
+        return (_usd * 100000000000000000000 / 14) +
+          (70000000 * (ln(_numerator * 10 ** 18) - ln(_denominator * 10 ** 18)));
+
+      } else {
+        return usd * 100000000000000000000 / 14;
+      }
+    } else {
+      _denominator = 7000000 + raised;
+      _numerator = _denominator + usd;
+      return 70000000 * (ln(_numerator * 10 ** 18) - ln(_denominator * 10 ** 18));
+    }
+  }
+
+  /**
+  * @dev Computes ln(x) with 18 artifical decimal places for input and output
+  * This algotihm uses a logarithm of the form
+    * ln(x) = ln(y * 1.5^k) = k ln(1.5) + ln(y)
+    * where ln(1.5) is a known value and ln(y) is computed with a tayilor series
+    * for 1 < y < 1.5 which is within radius of convergence of the Taylor series.
+    * https://en.wikipedia.org/wiki/Natural_logarithm#Derivative.2C_Taylor_series
+    */
+  function ln(uint256 x) internal pure returns(uint256) {
+    uint256 result = 0;
+    uint256 k = 0;
+    uint256 n = 1;
+    uint256 powY;
+    uint256 y;
+
+    while (x >= 1500000000000000000) {
+      k += 1;
+      x = x * 2 / 3; // same as x / 1.5
     }
 
-    modifier isOwner(){
-        require(msg.sender == ownerWallet);
-        _;
+    result = k * 405465000000000000;
+    y = x - 1000000000000000000;
+    powY = y;
+
+    while (n < 10) {
+      result = result + (powY / n);
+      n = n + 1;
+      powY = powY * y / 1000000000000000000;
+      result = result - (powY / n);
+      n = n + 1;
+      powY = powY * y / 1000000000000000000;
     }
+  }
 
-	function TokenSale (
-		uint256 _startTime,
-		uint256 _endTime,
-		address _foundationWallet,
-		address _corporationWallet,
-		address _rewardWallet,
-		address _teamWallet,
-		uint256 _startVesting
-	) 
-	    public
-		validAddress(_foundationWallet)
-		validAddress(_corporationWallet)
-		validAddress(_rewardWallet)
-		validAddress(_teamWallet)
-	{
-		require(_startTime > block.timestamp - 60);
-		require(_endTime > startTime);
-		require(_startVesting > startTime);
-
-		startVesting = _startVesting;
-		startTime = _startTime;
-		endTime = _endTime;
-		ownerWallet = msg.sender;
-		foundationWallet = _foundationWallet;
-		corpotationWallet = _corporationWallet;
-		rewardWallet = _rewardWallet;
-		teamWallet = _teamWallet;
-	}
-
-	/**
-	 * @dev deploy the token itself
-	 * @notice The owner of this contract is the owner of token's contract
-	 * @param _supply Token total supply
-	 * @param _ownerPool Percentage of tokens the owner assigns to himself
-	 */
-	function deploy (
-		uint256 _supply, 
-		uint256 _ownerPool,
-		address _vestingContract
-	) 
-	public isOwner returns (bool) 
-	{
-		require(!activated);
-		token = new SDT (
-			_supply, 
-			msg.sender, 
-			this,
-			_ownerPool
-		);
-		vesting = TokenVesting(_vestingContract);
-		activated = true;
-		return true;
-	}
-
-	function stop() public isOwner isActive returns (bool) {
-		isStopped = true;
-		return true;
-	}
-
-	function resume() public isOwner returns (bool) {
-		require(isStopped);
-		isStopped = false;
-		return true;
-	}
-
-	/**
-	 * @dev deploy the token itself
-	 * @notice The owner of this contract is the owner of token's contract
-	 * @param _usd amount invested in USD
-	 * @param _eth amount invested in ETH y contribution was made in ETH, 0 otherwise
-	 * @param _btc amount invested in BTC y contribution was made in BTC, 0 otherwise
-	 * @param _address Address to send tokens to
-	 * @param _vesting vesting finish timestamp
-	 * @param _discountBase a multiplier for tokens based on a discount choosen and a vesting time
-	 * @param _purchaseVestingStarts Vesting start timestamp
-	 */
-	function purchase (
-		uint256 _usd, 
-		uint256 _eth, 
-		uint256 _btc, 
-		address _address,
-		uint256 _discountBase,
-		uint256 _vesting,
-		uint256 _purchaseVestingStarts
-	) 
-		public
-		isActive
-		isOwner
-		validAddress(_address)
-		returns (uint256)
-	{
-		require(_usd >= 10);
-
-		uint256 soldAmount = computeTokens(_usd);
-		soldAmount = computeBonus(soldAmount, _discountBase);
-
-		if (_purchaseVestingStarts < startVesting){
-			_purchaseVestingStarts = startVesting;
-		}
-
-		grantVestedTokens(
-			_address, 
-			soldAmount, 
-			_purchaseVestingStarts, 
-			_vesting
-		);
-		updateStats(_usd, soldAmount);
-		NewBuyer(_address, soldAmount, _usd, _eth, _btc);
-
-		return soldAmount;
-	}
-
-	/**
-	 * @dev Helper function to update collected and allocated tokens stats
-	 */
-	function updateStats(uint256 usd, uint256 tokens) internal {
-		raised = raised + usd;
-		soldTokens = soldTokens + tokens;
-	}
-
-	/**
-	 * @dev Helper function to compute bonus amount
-	 * @param _amount number of toknes before bonus
-	 * @param _discountBase percentage of price after discount
-	 * @notice 80 <= dicountBase <= 100
-	 * @notice _discountBase is the resultant of (100 - discount) 
-	 */
-	function computeBonus(uint256 _amount, uint256 _discountBase) internal pure returns (uint256){
-		require (_discountBase >= 80);
-		require (_discountBase <= 100);
-		return _amount * 100 / _discountBase;
-	}
-
-	/**
-	* @dev Number of tokens is given by:
-	* 70,000,000 ln((7,000,000 + raised + usd) / (7,000,000 + raised))
-	*/
-	function computeTokens(uint256 usd) public constant returns (uint256) {
-		require (usd > 0);
-
-		uint256 _numerator;
-		uint256 _denominator;
-
-		if (raised < 7000000) {
-			if (usd + raised > 7000000) {
-
-				uint256 _usd = 7000000 - raised;
-				usd -= _usd;
-
-				_denominator = 14000000; // 7M+7M raised
-				_numerator = _denominator + usd;
-
-				return (_usd * 100000000000000000000 / 14) + 
-					(70000000 * (ln(_numerator * 10 ** 18) - ln(_denominator * 10 ** 18)));
-
-			} else {
-				return usd * 100000000000000000000 / 14;
-			}
-		} else {
-			_denominator = 7000000 + raised;
-			_numerator = _denominator + usd;
-			return 70000000 * (ln(_numerator * 10 ** 18) - ln(_denominator * 10 ** 18));
-		}
-	}
-
-	/**
-	* @dev Computes ln(x) with 18 artifical decimal places for input and output
-	* This algotihm uses a logarithm of the form
-	* ln(x) = ln(y * 1.5^k) = k ln(1.5) + ln(y)
-	* where ln(1.5) is a known value and ln(y) is computed with a tayilor series for
-	* 1 < y < 1.5 which is within radius of convergence of the Taylor series.
-	* https://en.wikipedia.org/wiki/Natural_logarithm#Derivative.2C_Taylor_series
-	*/
-	function ln(uint256 x) internal pure returns (uint256) {
-		uint256 result = 0;
-		uint256 k = 0;
-		uint256 n = 1;
-
-		uint256 powY;
-		uint256 y;
-
-		while (x >= 1500000000000000000){
-			k += 1;
-			x = x * 2 / 3; // same as x / 1.5
-		}
-
-		result = k * 405465000000000000;
-		y = x - 1000000000000000000;
-		powY = y;
-
-		while (n < 10){
-			result = result + (powY / n);
-			n = n + 1;
-			powY = powY * y / 1000000000000000000;
-			result = result - (powY / n);
-			n = n + 1;
-			powY = powY * y / 1000000000000000000;
-		}
-
-		return result;
-	}
-
-	/**
-	 * @dev deploy the token itself
-	 * @notice The owner of this contract is the owner of token's contract
-	 * @param _to Adress to grant vested tokens
-	 * @param _value number of tokens to grant
-	 * @param _start vesting start timestamp
-	 * @param _vesting vesting finish timestamp
-	 */
+  /**
+   * @dev deploy the token itself
+   * @notice The owner of this contract is the owner of token's contract
+   * @param _to Adress to grant vested tokens
+   * @param _value number of tokens to grant
+   * @param _start vesting start timestamp
+   * @param _vesting vesting finish timestamp
+   */
    function grantVestedTokens(
         address _to,
         uint256 _value,
         uint256 _start,
         uint256 _vesting
-    ) 
-        internal
-    {
-    	token.transfer(vesting, _value);
-        vesting.grantVestedTokens(
-	        _to,
-	        _value,
-	        _start,
-	        _vesting
-        );
+    ) internal {
+      token.transfer(vesting, _value);
+      vesting.grantVestedTokens(_to, _value, _start, _vesting);
     }
 }
--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -7,176 +7,169 @@ import './SendToken.sol';
  * @dev see https://send.sd/token
  */
 contract TokenVesting {
+  address public owner;
+  address public ico;
+  bool public initialized;
+  bool public active;
+  SendToken public token;
+  mapping (address => TokenGrant[]) public grants;
 
-    address public owner;
-    address public ico;
-    bool public initialized;
-    bool public active;
-    SendToken public token;
-    mapping (address => TokenGrant[]) public grants;
+  struct TokenGrant {
+    uint256 value;
+    uint256 claimed;
+    uint256 vesting;
+    uint256 start;
+  }
 
-    struct TokenGrant {
-        uint256 value;
-        uint256 claimed;
-        uint256 vesting;
-        uint256 start;
+  event NewTokenGrant (
+    address indexed to,
+    uint256 value,
+    uint256 start,
+    uint256 vesting
+  );
+
+  event NewTokenClaim (
+    address indexed holder,
+    uint256 value
+  );
+
+  modifier ownerRestricted() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  modifier icoResticted() {
+    require(msg.sender == ico);
+    _;
+  }
+
+  modifier isActive(){
+    require(active);
+    _;
+  }
+
+  function TokenVesting() public {
+    owner = msg.sender;
+    active = false;
+  }
+
+  function init(address _token, address _ico) public ownerRestricted {
+    require(!active);
+    require(!initialized);
+    token = SendToken(_token);
+    ico = _ico;
+    active = true;
+  }
+
+  function stop() public isActive ownerRestricted {
+    active = false;
+  }
+
+  function resume() public isActive ownerRestricted {
+    require(!active);
+    require(initialized);
+    active = true;
+  }
+
+  /**
+  * @dev Grant vested tokens.
+  * @notice Only for ICO contract address.
+  * @param _to Addres to grant tokens to.
+  * @param _value Number of tokens granted.
+  * @param _vesting Vesting finish timestamp.
+  * @param _start Vesting start timestamp.
+  */
+  function grantVestedTokens(
+      address _to,
+      uint256 _value,
+      uint256 _start,
+      uint256 _vesting
+  ) public icoResticted {
+    require (_value > 0);
+    require (_vesting > _start);
+    require (grants[_to].length < 10);
+
+    TokenGrant memory grant = TokenGrant(_value, 0, _vesting, _start);
+    grants[_to].push(grant);
+
+    NewTokenGrant(_to, _value, _start, _vesting);
+  }
+
+  /**
+  * @dev Calculate vested claimable tokens on current time
+  * @param _tokens Number of tokens granted
+  * @param _vesting Vesting finish timestamp
+  * @param _start Vesting start timestamp
+  * @param _claimed Number of tokens already claimed
+  */
+  function calculateVestedTokens(
+      uint256 _tokens,
+      uint256 _vesting,
+      uint256 _start,
+      uint256 _claimed
+  ) internal constant returns (uint256) {
+    uint256 time = block.timestamp;
+
+    if (time < _start) {
+      return 0;
     }
-
-    event NewTokenGrant (
-        address indexed to, 
-        uint256 value, 
-        uint256 start, 
-        uint256 vesting
+    if (time >= _start + _vesting) {
+      return SafeMath.sub(_tokens, _claimed);
+    }
+    uint256 vestedTokens = SafeMath.div(
+      SafeMath.mul(_tokens, SafeMath.sub(time, _start)),
+      SafeMath.sub(_vesting, _start)
     );
 
-    event NewTokenClaim (
-        address indexed holder, 
-        uint256 value
-    );
-    
-    modifier ownerRestricted() {
-        require(msg.sender == owner);
-        _;
+    return SafeMath.sub(vestedTokens, _claimed);
+  }
+
+  /**
+  * @dev Claim all vested tokens up to current date for myself
+  */
+  function claimTokens() public {
+    claimTokensFor(msg.sender);
+  }
+
+  /**
+  * @dev Claim all vested tokens up to current date in behaviour of an user
+  */
+  function claimTokensFor(address _to) public ownerRestricted {
+    claimTokensFor(_to);
+  }
+
+  /**
+  * @dev Claim all vested tokens up to current date
+  */
+  function claim(address _to) internal {
+    uint256 numberOfGrants = grants[_to].length;
+
+    if (numberOfGrants == 0) {
+      return;
     }
 
-    modifier icoResticted() {
-        require(msg.sender == ico);
-        _;
+    uint256 claimable = 0;
+    uint256 claimableFor = 0;
+    for (uint256 i = 0; i < numberOfGrants; i++) {
+      claimableFor = calculateVestedTokens (
+        grants[_to][i].value,
+        grants[_to][i].vesting,
+        grants[_to][i].start,
+        grants[_to][i].claimed
+      );
+      claimable = SafeMath.add (
+        claimable,
+        claimableFor
+      );
+      grants[_to][i].claimed = SafeMath.add (
+        grants[_to][i].claimed,
+        claimableFor
+      );
     }
 
-    modifier isActive(){
-        require(active);
-        _;
-    }
+    token.transfer(_to, claimable);
+    //totalSupply += claimable;
 
-    function TokenVesting() public {
-        owner = msg.sender;
-        active = false;
-    }
-
-    function init(address _token, address _ico) public ownerRestricted {
-        require(!active);
-        require(!initialized);
-        token = SendToken(_token);
-        ico = _ico;
-        active = true;
-    }
-
-    function stop() public isActive ownerRestricted {
-        active = false;
-    }
-    
-    function resume() public isActive ownerRestricted {
-        require(!active);
-        require(initialized);
-        active = true;
-    }
-
-    /**
-    * @dev Grant vested tokens.
-    * @notice Only for ICO contract address.
-    * @param _to Addres to grant tokens to.
-    * @param _value Number of tokens granted.
-    * @param _vesting Vesting finish timestamp.
-    * @param _start Vesting start timestamp.
-    */
-    function grantVestedTokens(
-        address _to,
-        uint256 _value,
-        uint256 _start,
-        uint256 _vesting
-    ) 
-        icoResticted
-        public 
-    {   
-        require (_value > 0);
-        require (_vesting > _start);
-        require (grants[_to].length < 10);
-
-        TokenGrant memory grant = TokenGrant(_value, 0, _vesting, _start);
-        grants[_to].push(grant);
-        
-        NewTokenGrant(_to, _value, _start, _vesting);
-    }
-
-    /**
-    * @dev Calculate vested claimable tokens on current time
-    * @param _tokens Number of tokens granted
-    * @param _vesting Vesting finish timestamp
-    * @param _start Vesting start timestamp
-    * @param _claimed Number of tokens already claimed
-    */
-    function calculateVestedTokens(
-        uint256 _tokens,
-        uint256 _vesting,
-        uint256 _start,
-        uint256 _claimed
-    ) 
-        internal constant returns (uint256)
-    {
-        uint256 time = block.timestamp;
-
-        if (time < _start) {
-            return 0;
-        }
-        if (time >= _start + _vesting) {
-            return SafeMath.sub(_tokens, _claimed);
-        }
-        uint256 vestedTokens = SafeMath.div(
-            SafeMath.mul(_tokens, SafeMath.sub(time, _start)),
-            SafeMath.sub(_vesting, _start)
-        );
-
-        return SafeMath.sub(vestedTokens, _claimed);
-    }
-
-    /**
-    * @dev Claim all vested tokens up to current date for myself
-    */
-    function claimTokens() public {
-        claimTokensFor(msg.sender);
-    }
-
-    /**
-    * @dev Claim all vested tokens up to current date in behaviour of an user
-    */
-    function claimTokensFor(address _to) public ownerRestricted {
-        claimTokensFor(_to);
-    }
-
-    /**
-    * @dev Claim all vested tokens up to current date
-    */
-    function claim(address _to) internal {
-        uint256 numberOfGrants = grants[_to].length;
-
-        if (numberOfGrants == 0) {
-            return;
-        }
-
-        uint256 claimable = 0;
-        uint256 claimableFor = 0;
-        for (uint256 i = 0; i < numberOfGrants; i++) {
-            claimableFor = calculateVestedTokens (
-                grants[_to][i].value,
-                grants[_to][i].vesting,
-                grants[_to][i].start,
-                grants[_to][i].claimed
-            );
-            claimable = SafeMath.add (
-                claimable, 
-                claimableFor
-            );
-            grants[_to][i].claimed = SafeMath.add (
-                grants[_to][i].claimed,
-                claimableFor
-            );
-        }
-
-        token.transfer(_to, claimable);
-        //totalSupply += claimable;
-
-        NewTokenClaim(_to, claimable);
-    }
-
+    NewTokenClaim(_to, claimable);
+  }
 }
