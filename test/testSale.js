d5348d72577b75ee0bbebd33100ea266301b4ff2
Marcio Abreu <3432864+maabreuc@users.noreply.github.com>
2017-11-28 20:35:47 -0500

    Extend token sale tests, check balances flow between vesting and sale contracts.

--- test/testSale.js
+++ test/testSale.js
@@ -12,6 +12,8 @@ contract("TokenSale", function(accounts) {
   let token;
   let bought;
   let error;
+  let allocated;
+  let collected;
   let maxError = 0.00001;
   let currentDate = math.floor(Date.now() / 1000);
 
@@ -52,6 +54,7 @@ contract("TokenSale", function(accounts) {
     assert.equal(await vesting.owner.call(), accounts[0]);
     assert.equal(await vesting.ico.call(), sale.address);
     assert.equal(await vesting.token.call(), await sale.token.call());
+    assert.equal(await token.balanceOf.call(vesting.address), 0);
   });
 
   it("should fail if purchasing less than min", async function() {
@@ -77,18 +80,40 @@ contract("TokenSale", function(accounts) {
 
     let granted = await vesting.totalVestedTokens.call({from: accounts[9]});
 
+    allocated = granted;
+    collected = 10;
+
     assert(error < bought.valueOf() * maxError);
     assert.equal(granted.valueOf(), bought.valueOf());
     assert.equal(newSaleBalance, saleBalance - granted);
     assert.equal(newCirculatingSupply.valueOf(), circulatingSupply.valueOf());
-    assert.equal(await sale.raised.call(), 10);
+    assert.equal(await sale.raised.call(), collected);
     assert.equal(await sale.soldTokens.call(), granted.valueOf());
   });
 
   it("6M USD at 0.14 - should return right amount with a maximum error of 0.001%", async function() {
+    //Calculate tokens
     bought = await sale.computeTokens.call(6000000);
-    await sale.purchase(6000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
     error = math.abs(bought.valueOf() - 6000000 / 0.14 * 10 ** 18);
+
+    //execute purchase
+    let circulatingSupply = await vesting.circulatingSupply.call();
+    let saleBalance = await token.balanceOf.call(sale.address);
+    await sale.purchase(6000000, 0, 0, accounts[9], 100, currentDate + 5000, 0);
+    let newCirculatingSupply = await vesting.circulatingSupply.call();
+    let newSaleBalance = await token.balanceOf.call(sale.address);
+
+    let granted = await vesting.totalVestedTokens.call({from: accounts[9]});
+
+    allocated = allocated.plus(bought);
+    collected += 6000000;
+
+    assert(error < bought.valueOf() * maxError);
+    assert.equal(granted.valueOf(), allocated.valueOf());
+    assert.equal(newSaleBalance.valueOf(), saleBalance - bought);
+    assert.equal(newCirculatingSupply.valueOf(), circulatingSupply.valueOf());
+    assert.equal(await sale.raised.call().valueOf(), collected);
+    assert.equal(await sale.soldTokens.call(), granted.valueOf());
 
     assert(error < bought.valueOf() * maxError);
   });
@@ -102,11 +127,26 @@ contract("TokenSale", function(accounts) {
       let val2 = 70000000 * math.log(1.07142928571) * 10 ** 18;
 
       bought = await sale.computeTokens.call(2000000);
-      await sale.purchase(2000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
-
       error = math.abs(bought.valueOf() - val1 - val2);
 
+      //execute purchase
+      let circulatingSupply = await vesting.circulatingSupply.call();
+      let saleBalance = await token.balanceOf.call(sale.address);
+      await sale.purchase(2000000, 0, 0, accounts[9], 100, currentDate + 5000, 0);
+      let newCirculatingSupply = await vesting.circulatingSupply.call();
+      let newSaleBalance = await token.balanceOf.call(sale.address);
+
+      let granted = await vesting.totalVestedTokens.call({from: accounts[9]});
+
+      allocated = allocated.plus(bought);
+      collected += 2000000;
+
       assert(error < bought.valueOf() * maxError);
+      assert.equal(granted.valueOf(), allocated.valueOf());
+      assert.equal(newSaleBalance.valueOf(), saleBalance.sub(bought).valueOf());
+      assert.equal(newCirculatingSupply.valueOf(), circulatingSupply.valueOf());
+      assert.equal(await sale.raised.call().valueOf(), collected);
+      assert.equal(await sale.soldTokens.call(), granted.valueOf());
     }
   );
 
@@ -117,10 +157,26 @@ contract("TokenSale", function(accounts) {
       let val1 = 70000000 * math.log(1.46666635556) * 10 ** 18;
 
       bought = await sale.computeTokens.call(7000000);
-      await sale.purchase(7000000, 0, 0, 0x1, 100, currentDate + 5000, 0);
       error = math.abs(bought.valueOf() - val1);
 
+      //execute purchase
+      let circulatingSupply = await vesting.circulatingSupply.call();
+      let saleBalance = await token.balanceOf.call(sale.address);
+      await sale.purchase(7000000, 0, 0, accounts[9], 100, currentDate + 5000, 0);
+      let newCirculatingSupply = await vesting.circulatingSupply.call();
+      let newSaleBalance = await token.balanceOf.call(sale.address);
+
+      let granted = await vesting.totalVestedTokens.call({from: accounts[9]});
+
+      allocated = allocated.plus(bought);
+      collected += 7000000;
+
       assert(error < bought.valueOf() * maxError);
+      assert.equal(granted.valueOf(), allocated.valueOf());
+      assert.equal(newSaleBalance.valueOf(), saleBalance.sub(bought).valueOf());
+      assert.equal(newCirculatingSupply.valueOf(), circulatingSupply.valueOf());
+      assert.equal(await sale.raised.call().valueOf(), collected);
+      assert.equal(await sale.soldTokens.call(), granted.valueOf());
     }
   );
 });
