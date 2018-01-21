3db2f7e60c4d31d45ca27ec4dd3575117885116b
Klaus Hott Vidal <klahott@gmail.com>
2018-01-20 23:51:59 -0300

    Adding safety Method to retrieve tokens or eth sent to the Proxy contracts when the ICO is no longer on

--- contracts/SaleProxy.sol
+++ contracts/SaleProxy.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.18;
 
 import './ITokenSale.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/token/ERC20Basic.sol';
 
-contract SaleProxy {
+contract SaleProxy is Ownable {
   uint256 public discountBase;
   uint256 public vestingTime;
   ITokenSale public saleContract;
@@ -24,4 +26,26 @@ contract SaleProxy {
   function btcPurchase(address _beneficiary, uint256 _btcValue) public {
     require(saleContract.btcPurchase.(msg.sender, vestingTime, discountBase));
   }
+
+  //////////
+  // Safety Methods
+  //////////
+
+  /// @notice This method can be used by the controller to extract mistakenly
+  ///  sent tokens to this contract.
+  /// @param _token The address of the token contract that you want to recover
+  ///  set to 0 in case you want to extract ether.
+  function claimTokens(address _token) public onlyOwner {
+    if (_token == 0x0) {
+      owner.transfer(this.balance);
+      return;
+    }
+
+    ERC20Basic token = ERC20Basic(_token);
+    uint256 balance = token.balanceOf(this);
+    token.transfer(owner, balance);
+    ClaimedTokens(_token, owner, balance);
+  }
+
+  event ClaimedTokens(address indexed _token, address indexed _controller, uint256 _amount);
 }
